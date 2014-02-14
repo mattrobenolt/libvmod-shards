5c551de47aa59c3432c61a1dbfe6057b41f418c8
Matt Robenolt <matt@ydekproductions.com>
2014-02-13 17:11:07 -0800

    Add stupid package.sh so I know how to do this

---
+++ package.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+set -x
+
+export VARNISH_VERSION=${VARNISH_VERSION:="3.0.5"}
+export VARNISHSRC=../varnish-${VARNISH_VERSION}
+export DEBIAN_VARNISH_SRC=$VARNISHSRC
+export VMODDIR=/usr/lib/varnish/vmods
+
+git submodule update
+./autogen.sh && ./configure && make
+dpkg-buildpackage -uc -us -b

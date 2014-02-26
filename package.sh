3931e38c677ce82483eef639fabccad920029252
Matt Robenolt <m@robenolt.com>
2014-02-26 04:02:34 -0800

    init while updating

--- package.sh
+++ package.sh
@@ -6,6 +6,6 @@ export VARNISHSRC=../varnish-${VARNISH_VERSION}
 export DEBIAN_VARNISH_SRC=$VARNISHSRC
 export VMODDIR=/usr/lib/varnish/vmods
 
-git submodule update
+git submodule update --init
 ./autogen.sh && ./configure && make
 dpkg-buildpackage -uc -us -b
